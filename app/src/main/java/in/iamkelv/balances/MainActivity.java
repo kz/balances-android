44e2c7fa99b8b3edc3253cfedadbfa220d495274
Kelvin <hello@iamkelv.in>
2015-02-17 13:52:48 +0000

    Create initial MainActivity view

--- app/src/main/java/in/iamkelv/balances/MainActivity.java
+++ app/src/main/java/in/iamkelv/balances/MainActivity.java
@@ -1,14 +1,10 @@
 package in.iamkelv.balances;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
-import in.iamkelv.balances.R;
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,28 +19,5 @@ public class MainActivity extends ActionBarActivity {
             MainActivity.this.startActivity(setupIntent);
             finish();
         }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
