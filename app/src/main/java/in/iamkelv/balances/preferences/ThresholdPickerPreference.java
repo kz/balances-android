3f8f8a65b6803595f2c0fa9ead65b6cf543723c6
Kelvin <hello@iamkelv.in>
2016-03-11 23:17:54 +0000

    Prepare ThresholdPickerPreference

---
+++ app/src/main/java/in/iamkelv/balances/preferences/ThresholdPickerPreference.java
@@ -0,0 +1,14 @@
+package in.iamkelv.balances.preferences;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class ThresholdPickerPreference extends DialogPreference {
+
+
+    public ThresholdPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
